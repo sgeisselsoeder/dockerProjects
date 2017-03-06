39c0fa282852bb0d5fce8d762ed7071cabfb1992
Stefan Geißelsöder <stefan.geisselsoeder@fau.de>
2017-03-06 22:21:04 +0100

    Added output of dockerized analyses to gitignore

--- multiscale/runMultiscaleAnalysis.sh
+++ multiscale/runMultiscaleAnalysis.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Reminder: All output will be stored in /output/. Please don't forget to run this container with \"-v /path/to/folder/you/want/output:/output/\" (if you haven't done so already)."
-
 cd /multiscale/
 
 externalInput="/input/events.txt"
@@ -10,12 +8,14 @@ then
 	echo "Using inputs from " ${externalInput}
 	cp $externalInput /multiscale/input/events.txt
 	echo "Executing evaluation without significance computation (the required pseudo experiments would take very long on a single machine)"
+	echo "Reminder: All output will be stored in /output/. Please don't forget to run this container with \"-v /path/to/folder/you/want/output:/output/\" (if you haven't done so already)."
 	./pipelines/pipelineLocalInputEvaluation.sh
 
 else
 	echo "Using default inputs (random distribution, three artificial sources)"
 	echo "If you want to use your own data, please supply a file mounted to " ${externalInput}
 	echo "Executing evaluation without significance computation (the required pseudo experiments would take very long on a single machine)"
+	echo "Reminder: All output will be stored in /output/. Please don't forget to run this container with \"-v /path/to/folder/you/want/output:/output/\" (if you haven't done so already)."
 	./pipelines/pipelineLocalArtificialSources.sh
 
 fi
@@ -25,4 +25,4 @@ mv ./output/scenarioEvaluation/hammerProjEquatorial.txt /output/
 mv ./output/scenarioEvaluation/*.pdf /output/
 chmod -R 777 /output/
 
-echo "All done! Thank you for your partience."
+echo "All done! Thank you for your patience."
