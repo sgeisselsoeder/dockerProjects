12c068b3331d40a06e9583c55e2f1b631e23ab38
Stefan Geißelsöder <stefan.geisselsoeder@fau.de>
2017-03-07 02:25:50 +0100

    changed the default input to the three sources scenario

--- multiscale/runDocker.sh
+++ multiscale/runDocker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 DATETAG=`date +%Y%m%d`
+# docker run -v `pwd`/externalOutput:/output multiscale:${DATETAG}
 docker run -v `pwd`/externalInput:/input -v `pwd`/externalOutput:/output multiscale:${DATETAG}
