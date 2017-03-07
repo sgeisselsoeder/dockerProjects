993c564622586620a34d53e8760b3a697e5e8775
Stefan Geisselsoeder <sgeisselsoeder@km3net.de>
2017-03-07 11:12:16 +0100

    Updated the readme, updated the names of the references, tiny wording in howDocker.tex, more explicit output for the multiscale docker image

--- multiscale/runMultiscaleAnalysis.sh
+++ multiscale/runMultiscaleAnalysis.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+externalInput="/input/events.txt"
+echo "If you haven't overwritten the file" ${externalInput}", the default will be used:"
+echo "A random distribution of 13283 events"
+echo "+ artificial source, 49 events, declination -52.5, right ascension 260, radius 3"
+echo "+ artificial source, 18 events, declination +4, right ascension 160, radius 1"
+echo "+ artificial source, 74 events, declination -22.5, right ascension 55, radius 5"
+
 cd /multiscale/
 
-externalInput="/input/events.txt"
 if [ -e $externalInput ]; 
 then 
 	echo "Using inputs from " ${externalInput}
@@ -12,7 +18,7 @@ then
 	./pipelines/pipelineLocalInputEvaluation.sh
 
 else
-	echo "Using default inputs (random distribution, three artificial sources)"
+	echo "Using default inputs"
 	echo "If you want to use your own data, please supply a file mounted to " ${externalInput}
 	echo "Executing evaluation without significance computation (the required pseudo experiments would take very long on a single machine)"
 	echo "Reminder: All output will be stored in /output/. Please don't forget to run this container with \"-v /path/to/folder/you/want/output:/output/\" (if you haven't done so already)."
