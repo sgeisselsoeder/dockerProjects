5fb2042138a8bee7dd15e17ccb3c17b0721bfd73
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2017-04-25 13:31:42 +0200

    Updates the new output folder names in gitignore. Container is called km3net/toyExample now by default.

--- toyExample/runDocker.sh
+++ toyExample/runDocker.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+DATETAG=`date +%Y%m%d`
+ANALYSISBASENAME=my_analysis
 # run the docker container with a mapping of the local output folder to the output folder in the container
 # NOTE: both MUST be absolute paths, relative paths are not allowed
-docker run -v `pwd`/externalOutput:/output my_analysis:v1
+docker run -v `pwd`/hostInput:/input -v `pwd`/hostOutput:/output km3net/${ANALYSISBASENAME}:${DATETAG}
