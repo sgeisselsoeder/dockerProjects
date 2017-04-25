3eb09facd8ae2689062d259cbe55f3762e7c3ee4
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2017-04-25 13:41:39 +0200

    In multiscale: Renames Dockerfile to dockerfile, externalInput to hostInput, externalOutput to hostOutput. The default image name now is km3net/multiscale. Adds a script runDocker.sh to run the default image.

--- multiscale/runDocker.sh
+++ multiscale/runDocker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 DATETAG=`date +%Y%m%d`
-# docker run -v `pwd`/externalOutput:/output multiscale:${DATETAG}
-docker run -v `pwd`/externalInput:/input -v `pwd`/externalOutput:/output multiscale:${DATETAG}
+#docker run -v `pwd`/hostOutput:/output km3net/multiscale:${DATETAG}
+docker run -v `pwd`/hostInput:/input -v `pwd`/hostOutput:/output km3net/multiscale:${DATETAG}
+#docker run -v `pwd`/hostInput:/input -v `pwd`/hostOutput:/output km3net/multiscale:20170318
